23464a4ab525b464463245e6099fcccc24a479d6
Muneeb1024 <muneeblodhi124@gmail.com>
2024-05-15 08:30:17 -0700

    Adding a number guessing game.

---
+++ main.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("\n\tWelcome to Codewithmuneeb-CLI-NUMBER-GUESSING-GAME\n");
+
+const randomNumber = Math.floor(Math.random() * 4 + 1);
+
+const ans = await inquirer.prompt([
+    {
+        name:"userGuessedNumber",
+        type:"number",
+        message:"Enter your guess number (Number Limit From 1 to 5):",
+    }
+])
+if (ans.userGuessedNumber === randomNumber){
+    console.log("Congratulation! your guess number is correct");
+    
+}
+else{
+    console.log("Sorry! Your guess number is wrong");
+}
+
